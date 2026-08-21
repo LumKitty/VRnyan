fbcfa67b275c7663057b2dd8b82db09de1b8568c
Lum <LumKitty@users.noreply.github.com>
2026-08-21 04:15:16 +0100

    Bump version to RC1

--- VNyan_Liv/VNyan_Handlers.cs
+++ VNyan_Liv/VNyan_Handlers.cs
@@ -8,7 +8,7 @@ using static VRnyan.Settings;
 
 namespace VRnyan {
     public class VNyan_Handlers : IVNyanPluginManifest, IButtonClickedHandler, ITriggerHandler {
-        private const string VersionString = "2.3-beta2";
+        private const string VersionString = "2.3-RC1";
         public string PluginName { get; } = SharedValues.PluginName;
         public string Version { get; } = VersionString;
         public string Title { get; } = SharedValues.PluginName + " " + VersionString;
